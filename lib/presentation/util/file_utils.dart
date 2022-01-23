b453f7f9d5f65a08604723dbe3e2d74844a1f06b
Murilo Aires <muriloaires1@gmail.com>
2022-01-23 19:30:51 -0300

    Added remaining mime types

--- lib/presentation/util/file_utils.dart
+++ lib/presentation/util/file_utils.dart
@@ -2,11 +2,16 @@ import 'package:mime/mime.dart';
 
 bool hasValidMimeType(String path) {
   final validMimeTypes = [
+    'image/png',
+    'image/gif',
     'image/jpg',
     'image/jpeg',
-    'image/jpe',
-    'image/png',
-    'image/webp'
+    'image/pjpeg',
+    'image/tiff',
+    'image/bmp',
+    'image/vnd.adobe.photoshop',
+    'image/vnd.microsoft.icon',
+    'image/webp',
   ];
   final mime = lookupMimeType(path);
   return validMimeTypes.contains(mime);
